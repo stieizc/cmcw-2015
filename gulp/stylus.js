f495c247cc2b0222723afaaee2902efd19b90416
Charlie Brown <stieizc.33@gmail.com>
2015-08-07 09:14:00 +0800

    Install stylus

---
+++ gulp/stylus.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('stylus', () => {
+  gulp.src('./src/styles/main.styl')
+    .pipe(sourcemaps.init({
+      loadMaps: true,
+      debug: true
+    }))
+    .pipe(stylus())
+    .on('error', gutil.log.bind(gutil, 'Stylus Error'))
+    .pipe(sourcemaps.write('./sourcemaps/'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch-stylus', ['stylus'], () => {
+  return gulp.watch('./src/styles/*.styl', ['stylus']);
+});
