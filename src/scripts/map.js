f3439baa44c18f87119916188d35248f65edec69
Charlie Brown <stieizc.33@gmail.com>
2015-08-11 14:39:02 +0800

    Better map size with vh&vw

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -15,8 +15,6 @@ function toggle(mapWrapper) {
       if (iframe.src !== mapURL) {
         iframe.src = mapURL;
       }
-      iframe.width = 0.8 * section.scrollWidth;
-      iframe.height = 0.6 * section.scrollHeight;
       mapWrapper.style.display = 'block';
     } else {
       mapWrapper.style.display = 'none';
